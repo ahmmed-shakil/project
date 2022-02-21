03391222553017f561d5b69f40006bb7520d2252
ahmmed-shakile <shakilahmmed252@gmail.com>
2022-02-22 02:18:54 +0600

    designsddded2

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -1,81 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import useCountdown from '../hooks/useCountdown';
 
 const Watch = () => {
 
-    const [timerDays, setTimerDays] = useState();
-    const [timerHours, setTimerHours] = useState();
-    const [timerMinutes, setTimerMinutes] = useState();
-    const [timerSeconds, setTimerSeconds] = useState();
+    const endTime = new Date().getTime() + 60000 * 2; // 2 minutes
+    const [timeLeft, setEndTime] = useCountdown(endTime);
 
-    let interval;
-
-    const startTimer = () => {
-        const countDownDate = new Date("March 1,2022").getTime();
-
-        interval = setInterval(() => {
-            const now = new Date().getTime();
-
-            const distance = countDownDate - now;
-
-            const days = Math.floor(distance / (24 * 60 * 60 * 1000));
-            const hours = Math.floor(
-                (distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
-            );
-            const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
-            const seconds = Math.floor((distance % (60 * 1000)) / 1000);
-
-            if (distance < 0) {
-                // Stop Timer
-
-                clearInterval(interval.current);
-            } else {
-                // Update Timer
-                setTimerDays(days);
-                setTimerHours(hours);
-                setTimerMinutes(minutes);
-                setTimerSeconds(seconds);
-            }
-        });
-    };
-
-    useEffect(() => {
-        startTimer();
-    });
-
+    const minutes = Math.floor(timeLeft / 60000) % 60;
+    const seconds = Math.floor(timeLeft / 1000) % 60;
     return (
         <div className='background'>
             <h2 className='header watch'>Days Until Launch</h2>
             <hr className='border-bottom' />
-            <section className="timer-container">
-                <section className="timer">
-                    <div className="clock">
-                        <section>
-                            <span>{timerDays}</span>
-                            <small>Days</small>
-                        </section>
-                        <span>:</span>
-                        <section>
-                            <span>{timerHours}</span>
-                        </section>{" "}
-                        <span>:</span>
-                        <section>
-                            <span>{timerMinutes}</span>
-                        </section>{" "}
-                        <span>:</span>
-                        <section>
-                            <span>{timerSeconds}</span>
-                        </section>
-                    </div>
-                </section>
-            </section>
-        </div>
+            <p className='clock'>{`${minutes}:${seconds}`}</p>        </div>
     );
-};
-Watch.defaultProps = {
-    timerDays: 10,
-    timerHours: 10,
-    timerMinutes: 10,
-    timerSeconds: 10,
 };
 
 export default Watch;
